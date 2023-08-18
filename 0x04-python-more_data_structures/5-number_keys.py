36f1fc78e156111553c2228c5de2014b31660e41
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-08-18 08:02:00 -0700

    5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+  """
+  Returns the number of keys in a dictionary.
+
+  Args:
+    a_dictionary: The dictionary to count the keys of.
+
+  Returns:
+    The number of keys in the dictionary.
+  """
+
+  return len(a_dictionary)
+
+
