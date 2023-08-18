dfd6b9acc8645aba37099452479ce68b7ea4a520
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-08-18 08:03:44 -0700

    5

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
+if key in a_dictionary:
     a_dictionary[key] = value
-
-# Example usage
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-update_dictionary(my_dict, 'b', 5)
-update_dictionary(my_dict, 'd', 4)
-print(my_dict)  # Output: {'a': 1, 'b': 5, 'c': 3, 'd': 4}
-
+  else:
+    a_dictionary[key] = value
